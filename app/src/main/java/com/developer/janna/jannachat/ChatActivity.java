bd52d6f60384109ee700b98f37ba1fba5bfa5c9f
jannaescur <janna.escur@alu-etsetb.upc.edu>
2016-12-02 17:54:37 +0100

    Info evento + chat (pantalles separades)

---
+++ app/src/main/java/com/developer/janna/jannachat/ChatActivity.java
@@ -0,0 +1,77 @@
+package com.developer.janna.jannachat;
+
+import android.app.Activity;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+
+public class ChatActivity extends Activity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private ImageButton buttonSend;
+    private boolean side = false;
+    private Button buttonChat;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pantalla_chat);
+
+
+        buttonSend = (ImageButton) findViewById(R.id.send);
+        listView = (ListView) findViewById(R.id.msgview);
+
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.msg);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage(true);
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (chatText.getText().toString().trim().length() > 0){ /*miro si hi ha text per enviar*/
+                    sendChatMessage(true);
+                }
+            }
+        });
+
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+    }
+
+    private boolean sendChatMessage(Boolean esMio) {
+
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+        side=!side;
+        chatText.setText("");
+        return true;
+    }
+}
